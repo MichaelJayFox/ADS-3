c3d4110902f08ff3fbeede92af0124b9a2d44d47
MichaelJayFox <125122867+MichaelJayFox@users.noreply.github.com>
2023-03-12 19:34:15 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,6 +1,26 @@
 // Copyright 2021 NNTU-CS
 
 int cbinsearch(int *arr, int size, int value) {
-  //  поместить сюда реализацию алгоритма
+  int left, right=size - 1, cnt = 0, middle, i = 1;
+  while (left <= right){
+    middle = (left + right) / 2;
+    if (value < arr[middle]) {
+            right = middle - 1;
+        } else if (value > arr[middle]) {
+            left = middle + 1;
+        } else {
+            cnt++;
+            while (arr[i] == arr[mid - i]) {
+                count++;
+                i++;
+            }
+            i = 1;
+            while (arr[i] == arr[middle + i]) {
+                cnt++;
+                i++;
+            }
+            return cnt;
+        }
+    }
   return 0; // если ничего не найдено
 }
